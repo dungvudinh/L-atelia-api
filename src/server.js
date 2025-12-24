c6cf6c7d45f7f37acefc3e96a9692656cd4cc374
unknown <anhkho881@gmail.com>
2025-12-24 22:03:51 +0700

    ađ cors

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import initializeDatabase from './scripts/initialDatabase.js';
 
-const allowOrigins = ['http://localhost:5173', 'http://localhost:5174','https://latelia.com', 'https://admin.latelia.com' ];
+const allowOrigins = ['http://localhost:5173', 'http://localhost:5174','http://localhost:4173','http://localhost:4174', 'https://latelia.com', 'https://admin.latelia.com',  ];
 const app = express();
 
 const START_SERVER = () => {
